ad369d1674eb2c5da00900d6d989c0e7271803f9
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-14 18:01:01 +0530

    Add files via upload

---
+++ NonRepeating.java
@@ -0,0 +1,18 @@
+package com.operators;
+import java.util.*;
+import java.io.*;
+// XOR all the numbers
+//Q- Find the non repeating element in the array
+
+public class NonRepeating {
+    public static void main(String[] args){
+        int[] arr ={2,3,4,3,4,2,6,1};
+
+        int un =0;
+
+        for(int k : arr){
+            un^=k;
+        }
+System.out.println(un);
+    }
+}
