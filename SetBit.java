99a9477c93f7443923b6e234bf19244aaee92662
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-15 19:05:20 +0530

    Add files via upload

---
+++ SetBit.java
@@ -0,0 +1,17 @@
+package com.operators;
+
+public class SetBit {
+    public static void main(String[] args) {
+        int n = 44;
+        int count =0;
+        System.out.println(Integer.toBinaryString(n));// gives the binary representation of the number
+       while(n>0){
+
+           if((n&1) ==1)
+               count++;
+
+           n=n>>1;
+       }
+       System.out.println(count);
+    }
+}
