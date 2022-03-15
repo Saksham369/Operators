f641b136eb1cdc24ccf75981ca4245f525a7b246
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-15 17:39:26 +0530

    Add files via upload

---
+++ PascalTri.java
@@ -0,0 +1,10 @@
+package com.operators;
+
+public class PascalTri {
+    //Q- Find the sum of the nth row
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(1<<(n-1));
+
+    }
+}
