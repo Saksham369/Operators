a90fdd0586e27344fc560c6f0b6c0a2a103a1001
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-15 17:15:13 +0530

    Add files via upload

---
+++ NumOfDigits.java
@@ -0,0 +1,10 @@
+package com.operators;
+
+public class NumOfDigits {
+    public static void main(String[] args){
+        int n = 4;
+        int b = 2;
+        int ans = (int)(Math.log(n)/Math.log(b))  + 1;
+        System.out.println(ans);
+    }
+}
