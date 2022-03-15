2a7153f9e5b3893015f448d9c5fe5c86115f94c8
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-15 17:39:51 +0530

    Update PascalTri.java

--- PascalTri.java
+++ PascalTri.java
@@ -1,7 +1,7 @@
 package com.operators;
 
 public class PascalTri {
-    //Q- Find the sum of the nth row
+    //Q- Find the sum of the nth row in the pascal triangle
     public static void main(String[] args){
         int n = 5;
         System.out.println(1<<(n-1));
