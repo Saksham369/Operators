738944a291f4a0f4b4d99cc2c7500c24cc07bea7
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-15 11:57:01 +0530

    Add files via upload

---
+++ SetIthBit.java
@@ -0,0 +1,9 @@
+package com.operators;
+
+public class SetIthBit {
+    //Q- Set the ith bit and turn it to 1
+    public static void main(String[] args){
+        int n = 6;
+        System.out.println(n|(1<<(2)));
+    }
+}
