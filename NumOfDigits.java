b71d75444543a1e1621a657777c98965e22271b2
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-15 17:24:37 +0530

    Update NumOfDigits.java

--- NumOfDigits.java
+++ NumOfDigits.java
@@ -1,6 +1,8 @@
 package com.operators;
 
 public class NumOfDigits {
+    //Time Complexity O(log n)
+    //Find the number of digits of a number
     public static void main(String[] args){
         int n = 4;
         int b = 2;
